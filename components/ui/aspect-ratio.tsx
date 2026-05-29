397ab744e1994fde71431fbc8ee1e066f5228f6f
Raviraj Sahu <ravirajsahu312@gmail.com>
2026-05-29 11:49:03 +0530

    build fix done..

--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-import { cn } from "@/lib/utils"
-
-function AspectRatio({
-  ratio,
-  className,
-  ...props
-}: React.ComponentProps<"div"> & { ratio: number }) {
-  return (
-    <div
-      data-slot="aspect-ratio"
-      style={
-        {
-          "--ratio": ratio,
-        } as React.CSSProperties
-      }
-      className={cn("relative aspect-(--ratio)", className)}
-      {...props}
-    />
-  )
-=======
 'use client'
 
 import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio'
 
-function AspectRatio({
-  ...props
-}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
-  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />
->>>>>>> 3ac58cbef2fa2e7ef684a927d42e1d6e191d5545
+function AspectRatio(
+  props: React.ComponentProps<typeof AspectRatioPrimitive.Root>
+) {
+  return (
+    <AspectRatioPrimitive.Root
+      data-slot="aspect-ratio"
+      {...props}
+    />
+  )
 }
 
 export { AspectRatio }
